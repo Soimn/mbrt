183e817a11fcf4692a9958cf9b7dfcf995efe395
soimn <soimn.dok@gmail.com>
2024-05-15 20:41:18 +0200

    16 color grayscale

--- mbrt.asm
+++ mbrt.asm
@@ -5,6 +5,28 @@ org 0x7c00
 	mov ax, 0x0012
 	int 0x10
 
+	; Set DAC registers to interesting colors
+	xor bx, bx
+	xor cx, cx
+	xor dx, dx
+	mov ax, 0x1010
+	set_dac_loop:
+		int 0x10
+		add cx, 0x0404
+		add dh, 0x04
+		inc bx
+		cmp bx, 16
+		jb set_dac_loop
+
+	; Set palette
+	mov ax, 0x1000
+	xor bx, bx
+	set_palette:
+		int 0x10
+		add bx, 0x0101
+		cmp bx, 0x1010
+		jb set_palette
+
 	mov ax, 0xA000
 	mov es, ax
 
@@ -23,24 +45,26 @@ org 0x7c00
 			
 			call rand_01
 
-			fild word [constant_3]
+			fild word [constant_15]
 			fmulp
 			fistp word [float_xchg]
 
-			mov bx, [float_xchg]
-			and bx, 0x3
-			mov ax, [palette+bx]
+			mov al, byte [float_xchg]
+
+			; TODO: for testing
+			mov al, dl
+			shr al, 4
 
 			mov ah, 0x0C
 			mov bh, 0
 			int 0x10
 
 			inc cx
-			cmp cx, 640
+			cmp cx, WIDTH
 			jb x_loop
 
 		inc dx
-		cmp dx, 480
+		cmp dx, HEIGHT
 		jb y_loop
 
 	jmp $
@@ -49,9 +73,11 @@ rand_01:
 	fldln2
 	ret
 
-	constant_3: dw 3
+	WIDTH equ 640
+	HEIGHT equ 480
+
+	constant_15: dw 15
 	float_xchg: dw 0
-	palette: db 0x0, 0x8, 0x7, 0xF
 
 times 510-($-$$) db 0
 dw 0xAA55
