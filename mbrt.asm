2ae10ed37387d32141fa8cf717e3b71173924edd
soimn <soimn.dok@gmail.com>
2024-05-19 02:35:27 +0200

    scene

--- mbrt.asm
+++ mbrt.asm
@@ -42,7 +42,7 @@ org 0x7c00
 	xor dx, dx
 	fld dword [ray_y_origin]
 	y_loop:
-		xor cx, cx
+		xor di, di
 		fld1
 		fchs
 		x_loop:
@@ -55,41 +55,23 @@ org 0x7c00
 			fld1
 			fchs ; v_z = -1
 
-			fldz ; q_x = 0
-			fldz ; q_y = 0
-
-			fld1
-			fadd st0, st0
-			fadd st0, st0
-			fchs ; q_z = -4
-
-			fld1 ; r_sq = 1
-			fstp dword [ray_sphere_args_r_sq]
-
-			call ray_sphere
-			
-			cmp al, 1
-			je color_set_black
-				fld1
-				jmp color_set_end
-			color_set_black:
-				fldz
-			color_set_end:
+			call trace_ray
 			
 			fild word [constant_15]
 			fmulp
-			fistp word [light_to_color_float_xchg]
+			fistp word [lambda_0]
 
 			mov ah, 0x0C
-			mov al, byte [light_to_color_float_xchg]
+			mov al, byte [lambda_0]
 			mov bh, 0
+			mov cx, di
 			int 0x10
 
 			frstor [ss:bp]
 
 			fadd dword [ray_step]
-			inc cx
-			cmp cx, 640
+			inc di
+			cmp di, 640
 			jb x_loop
 
 		fincstp
@@ -115,7 +97,7 @@ rand_01:
 ; v_x, v_y, v_z, q_x, q_y, q_z
 ; ->
 ; v_x, v_y, v_z, t
-ray_sphere:
+test_sphere:
 
 	; Compute <v, q>
 	; fpu stack
@@ -142,19 +124,18 @@ ray_sphere:
 	fxch
 	fmul st0, st0
 	faddp
-	fsub dword [ray_sphere_args_r_sq]
+	fsub dword [lambda_0]
 	; fpu stack
 	; v_x, v_y, v_z, <v, q>, <q, q> - r^2
 	
 	; Compute <v, q>/<v, v> and (<q, q> - r^2)/<v, v>
 	; fpu stack
 	; v_x, v_y, v_z, <v, q>, <q, q> - r^2
-	fld st4
-	fmul st0, st0
-	fld st4
-	fmul st0, st0
-	fld st4
-	fmul st0, st0
+	mov cx, 3
+	div_loop:
+		fld st4
+		fmul st0, st0
+	loop div_loop
 	faddp
 	faddp
 	fdiv st2, st0
@@ -172,16 +153,67 @@ ray_sphere:
 	; fpu stack
 	; v_x, v_y, v_z, <v, q>/<v, v>, sqrt((<v, q>/<v, v>)^2 - (<q, q> - r^2)/<v, v>)
 
-	; Compute t
+	; Compute t and update min_t
 	; fpu stack
 	; v_x, v_y, v_z, <v, q>/<v, v>, sqrt((<v, q>/<v, v>)^2 - (<q, q> - r^2)/<v, v>)
 	fsubp
 	ftst
 	fstsw ax
-	or ah, al
+	fnclex
+	or al, ah
 	and al, 1
+	cmp al, 0
+	jne test_sphere_no_hit
+	fld dword [trace_ray_min_t]
+	fcomp st1
+	fstsw ax
+	and ah, 1
+	cmp ah, 0
+	jne test_sphere_no_hit
+	fst dword [trace_ray_min_t]
+	mov bx, si
+	test_sphere_no_hit:
+	fincstp
 	; fpu stack
-	; v_x, v_y, v_z, t
+	; v_x, v_y, v_z
+
+	ret
+
+trace_ray:
+	mov bx, 0
+	fld dword [ground_r_sq]
+	fstp dword [trace_ray_min_t]
+
+	mov si, 1
+	fldz  ; q_x  = 0
+	fldz  ; q_y  = 0
+	fldpi
+	fadd st0, st0
+	fchs  ; q_z  = -2pi
+	fldpi ; r_sq = pi
+	fstp dword [lambda_0]
+	call test_sphere
+
+	mov si, 2
+	fldz                    ; q_x = 0
+	fld dword [ground_y]    ; q_y  = $ground_y
+	fldz                    ; q_z  = 0
+	fld dword [ground_r_sq] ; r_sq = $ground_r_sq
+	fstp dword [lambda_0]
+	call test_sphere
+
+	cmp bx, 0
+	je hit_sky
+		cmp bx, 1
+		jne hit_ground
+			fld1
+			jmp hit_end
+		hit_ground:
+			fldz
+			jmp hit_end
+	hit_sky:
+		fldln2
+	hit_end:
 
 	ret
 
@@ -189,12 +221,15 @@ ray_sphere:
 	ray_step: dd 0.003125
 
 	constant_15: dw 15
-	light_to_color_float_xchg: dd 0xFFFFFFFF
 
 	rand_01_seed: dd 2.718281828459045
 	rand_01_phi: dd 1.618033988749894848204586834365638117
 
-	ray_sphere_args_r_sq: dd 0
+	lambda_0: dd 0
+
+	ground_r_sq: dd 1.0e6
+	ground_y: dd -1001.752714447281309590393390427036516651063469065
+	trace_ray_min_t: dd 0
 
 times 510-($-$$) db 0
 dw 0xAA55
